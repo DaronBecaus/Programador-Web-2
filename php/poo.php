1f27bf43d8931de8dc0c6ee0fba65d6f124c226c
DaronBecaus <126109661+DaronBecaus@users.noreply.github.com>
2023-03-17 23:42:18 -0300

    model php

--- php/poo.php
+++ php/poo.php
@@ -1,34 +1,6 @@
 <?php
 require_once $_SERVER["DOCUMENT_ROOT"] . "/Programador-Web-2/php/cabecalho.php";
-
-class Carro
-{ //define uma classe em php
-    public $cor; //propriedades são características que você deseja representar
-    public $qtd_portas;
-    private $modelo;
-    public function buzinar()
-    { //métodos são ações que você deseja que os objetos realizem
-        echo "bi";
-    }
-    //definindo um método construtor para criação de objetos com propriedades instanciadas
-    //nesse caso o valor padrão para os para metros e null para tornar a criação do objeto com seu valor definido opcional
-    function __construct($cor = null, $qtd_portas = null)
-    {
-        $this->cor = $cor;
-        //a palavra this refere-se a propriedade da classe definida la no incio então $this->cor != cor
-        //$this->cor = propriedade da classe
-        //$cor = parâmetro utilizado no construtor
-        $this->qtd_portas = $qtd_portas;
-    }
-    public function getModelo()
-    { //função getter que retorna o valor de uma propriedade
-        return $this->modelo;
-    }
-    public function setModelo($modelo)
-    { //função setter que define o valor de uma propriedade
-        $this->modelo = $modelo;
-    }
-}
+require_once $_SERVER["DOCUMENT_ROOT"] . "/Programador-Web-2/php/carro.php";
 
 $carro = new Carro(); //instanciando um objeto dessa classe
 echo $carro->cor; //acessando uma propriedade do objeto
